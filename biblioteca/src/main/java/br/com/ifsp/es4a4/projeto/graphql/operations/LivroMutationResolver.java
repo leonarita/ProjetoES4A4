f49eb2677743b3564ff7c9943987c2384ef6eebd
Leonardo <leo_narita@hotmail.com>
2021-03-21 01:09:13 -0300

    Comentando classes que impossibilitam testes automatizados

--- biblioteca/src/main/java/br/com/ifsp/es4a4/projeto/graphql/operations/LivroMutationResolver.java
+++ biblioteca/src/main/java/br/com/ifsp/es4a4/projeto/graphql/operations/LivroMutationResolver.java
@@ -1,24 +1,24 @@
-package br.com.ifsp.es4a4.projeto.graphql.operations;
-
-import org.springframework.stereotype.Component;
-import org.springframework.validation.annotation.Validated;
-
-import br.com.ifsp.es4a4.projeto.graphql.mapper.CriarLivroInput;
-import br.com.ifsp.es4a4.projeto.graphql.mapper.LivroDto;
-import br.com.ifsp.es4a4.projeto.graphql.mapper.LivroMapper;
-import br.com.ifsp.es4a4.projeto.service.LivroService;
-import graphql.kickstart.tools.GraphQLMutationResolver;
-import lombok.RequiredArgsConstructor;
-
-@Validated
-@Component
-@RequiredArgsConstructor
-public class LivroMutationResolver implements GraphQLMutationResolver {
-	
-	private final LivroService livroService;
-	
-	public LivroDto criarLivro(@Validated CriarLivroInput input) {
-		return LivroMapper.entityToDto(this.livroService.create(LivroMapper.dtoToEntity(input)));
-	}
-
-}
+//package br.com.ifsp.es4a4.projeto.graphql.operations;
+//
+//import org.springframework.stereotype.Component;
+//import org.springframework.validation.annotation.Validated;
+//
+//import br.com.ifsp.es4a4.projeto.graphql.mapper.CriarLivroInput;
+//import br.com.ifsp.es4a4.projeto.graphql.mapper.LivroDto;
+//import br.com.ifsp.es4a4.projeto.graphql.mapper.LivroMapper;
+//import br.com.ifsp.es4a4.projeto.service.LivroService;
+//import graphql.kickstart.tools.GraphQLMutationResolver;
+//import lombok.RequiredArgsConstructor;
+//
+//@Validated
+//@Component
+//@RequiredArgsConstructor
+//public class LivroMutationResolver implements GraphQLMutationResolver {
+//	
+//	private final LivroService livroService;
+//	
+//	public LivroDto criarLivro(@Validated CriarLivroInput input) {
+//		return LivroMapper.entityToDto(this.livroService.create(LivroMapper.dtoToEntity(input)));
+//	}
+//
+//}
--- biblioteca/src/main/java/br/com/ifsp/es4a4/projeto/graphql/operations/UploadFileMutation.java
+++ biblioteca/src/main/java/br/com/ifsp/es4a4/projeto/graphql/operations/UploadFileMutation.java
@@ -1,50 +1,50 @@
-package br.com.ifsp.es4a4.projeto.graphql.operations;
-
-import graphql.kickstart.servlet.context.DefaultGraphQLServletContext;
-import graphql.kickstart.tools.GraphQLMutationResolver;
-import graphql.schema.DataFetchingEnvironment;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Paths;
-import java.util.UUID;
-import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-
-import br.com.ifsp.es4a4.projeto.utils.files.StorageFile;
-
-@Slf4j
-@Component
-public class UploadFileMutation implements GraphQLMutationResolver {
-	
-	private final StorageFile storageFile;
-	
-	@Autowired
-	public UploadFileMutation(@Qualifier("files") StorageFile storageFile) {
-		this.storageFile = storageFile;
-	}
-	
-	public UUID uploadFile(DataFetchingEnvironment environment) {
-		DefaultGraphQLServletContext context = environment.getContext();
-		
-		context.getFileParts().forEach(part -> {
-			log.info("uploading: {} , size: {}", part.getSubmittedFileName(), part.getSize());
-			
-			String filename = Paths.get(part.getSubmittedFileName()).getFileName().toString();
-			
-			try (InputStream fileContent = part.getInputStream()) {
-				storageFile.createFile(fileContent, filename);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			
-		});
-		
-		return UUID.randomUUID();
-	}
-
-}
+//package br.com.ifsp.es4a4.projeto.graphql.operations;
+//
+//import graphql.kickstart.servlet.context.DefaultGraphQLServletContext;
+//import graphql.kickstart.tools.GraphQLMutationResolver;
+//import graphql.schema.DataFetchingEnvironment;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.nio.file.Paths;
+//import java.util.UUID;
+//import lombok.extern.slf4j.Slf4j;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.stereotype.Component;
+//
+//import br.com.ifsp.es4a4.projeto.utils.files.StorageFile;
+//
+//@Slf4j
+//@Component
+//public class UploadFileMutation implements GraphQLMutationResolver {
+//	
+//	private final StorageFile storageFile;
+//	
+//	@Autowired
+//	public UploadFileMutation(@Qualifier("files") StorageFile storageFile) {
+//		this.storageFile = storageFile;
+//	}
+//	
+//	public UUID uploadFile(DataFetchingEnvironment environment) {
+//		DefaultGraphQLServletContext context = environment.getContext();
+//		
+//		context.getFileParts().forEach(part -> {
+//			log.info("uploading: {} , size: {}", part.getSubmittedFileName(), part.getSize());
+//			
+//			String filename = Paths.get(part.getSubmittedFileName()).getFileName().toString();
+//			
+//			try (InputStream fileContent = part.getInputStream()) {
+//				storageFile.createFile(fileContent, filename);
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//			
+//			
+//		});
+//		
+//		return UUID.randomUUID();
+//	}
+//
+//}
