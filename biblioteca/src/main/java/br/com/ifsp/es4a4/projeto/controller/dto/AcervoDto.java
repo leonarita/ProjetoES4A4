b1c1d4e3e321eee880dd6bb682b87881a0658d53
Leonardo <leo_narita@hotmail.com>
2021-04-05 11:12:30 -0300

    Ajustes Redis

--- biblioteca/src/main/java/br/com/ifsp/es4a4/projeto/controller/dto/AcervoDto.java
+++ biblioteca/src/main/java/br/com/ifsp/es4a4/projeto/controller/dto/AcervoDto.java
@@ -1,17 +1,20 @@
 package br.com.ifsp.es4a4.projeto.controller.dto;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@SuppressWarnings("serial")
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-public class AcervoDto {
+public class AcervoDto implements Serializable {
 
 	private Long id;
 	
